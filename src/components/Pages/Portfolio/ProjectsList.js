127e718e79b577a5884c4d2c3e9281e2bdd9a802
Pavel Naumovich <pavelnaumovic@gmail.com>
2023-09-12 22:05:24 +0100

    few weaks

--- src/components/Pages/Portfolio/ProjectsList.js
+++ src/components/Pages/Portfolio/ProjectsList.js
@@ -3,6 +3,7 @@ import theSnakeGameImage from "../../../img/snake.png";
 import cityWorndererImage from "../../../img/citywonderer.png";
 import javaScriptQuizPic from "../../../img/javascriptquiz.png";
 import passwordGeneratorImage from "../../../img/passwordGenerator.png";
+import portfolioImage from "../../../img/portimage.png";
 
 export const projects = [
   {
@@ -32,49 +33,25 @@ export const projects = [
   {
     image: javaScriptQuizPic,
     title: "JavaScript Quiz",
-    github: "https://github.com/PavsterUK/codiing-quiz-js",
+    github: "https://github.com/PavsterUK/javascript-quiz",
     tools: ["HTML", "CSS", "JavaScript"],
     descr: "JavaScript Quiz",
-    page: "https://pavsteruk.github.io/codiing-quiz-js/",
+    page: "https://pavsteruk.github.io/javascript-quiz/",
   },
   {
     image: passwordGeneratorImage,
     title: "Password Generator",
-    github: "https://github.com/PavsterUK/module-05-challenge",
+    github: "https://github.com/PavsterUK/password-generator",
     tools: ["HTML", "CSS", "JavaScript"],
     descr: "Password generator.",
-    page: "https://pavsteruk.github.io/module-05-challenge/",
+    page: "https://pavsteruk.github.io/password-generator/",
   },
   {
-    image: passwordGeneratorImage,
-    title: "Password Generator",
-    github: "https://github.com/PavsterUK/module-05-challenge",
+    image: portfolioImage,
+    title: "React Portfolio",
+    github: "https://github.com/PavsterUK/react-portfolio",
     tools: ["HTML", "CSS", "JavaScript"],
-    descr: "Password generator.",
-    page: "https://pavsteruk.github.io/module-05-challenge/",
-  },
-  {
-    image: theSnakeGameImage,
-    title: "The Snake Game",
-    github: "https://github.com/PavsterUK/snake-js",
-    tools: ["HTML", "CSS", "JavaScript"],
-    descr: "Little hobby project",
-    page: "https://pavsteruk.github.io/snake-js/",
-  },
-  {
-    image: theSnakeGameImage,
-    title: "The Snake Game",
-    github: "https://github.com/PavsterUK/snake-js",
-    tools: ["HTML", "CSS", "JavaScript"],
-    descr: "Little hobby project",
-    page: "https://pavsteruk.github.io/snake-js/",
-  },
-  {
-    image: theSnakeGameImage,
-    title: "The Snake Game",
-    github: "https://github.com/PavsterUK/snake-js",
-    tools: ["HTML", "CSS", "JavaScript"],
-    descr: "Little hobby project",
-    page: "https://pavsteruk.github.io/snake-js/",
+    descr: "My Portfolio using React.",
+    page: "https://pavsteruk.github.io/react-portfolio/#/",
   },
 ];
